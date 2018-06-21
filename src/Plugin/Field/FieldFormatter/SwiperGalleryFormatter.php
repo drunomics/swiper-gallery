4d8e0b0d26a0c35c62c5869832179f9586c1d293
Mathias Mueller <mathias.mueller@drunomics.com>
2018-06-21 10:46:00 +0200

    Add breakpoint config.

--- src/Plugin/Field/FieldFormatter/SwiperGalleryFormatter.php
+++ src/Plugin/Field/FieldFormatter/SwiperGalleryFormatter.php
@@ -48,7 +48,7 @@ class SwiperGalleryFormatter extends EntityReferenceFormatterBase implements Con
   protected $moduleHandler;
 
   /**
-   * The media gallery
+   * The media gallery.
    *
    * @var \Drupal\media\Entity\Media
    */
@@ -94,16 +94,17 @@ class SwiperGalleryFormatter extends EntityReferenceFormatterBase implements Con
    */
   public static function defaultSettings() {
     return [
-        'launcher_main_text' => 'Start gallery',
-        'launcher_footer_text' => 'Show all',
-        'show_preview_headline' => FALSE,
-        'preview_footer' => 'imageonly',
-        'image_style_preview_image' => 'swiper_gallery_preview',
-        'image_style_preview_thumbnail' => 'swiper_gallery_preview_thumbnail',
-        'image_style_gallery_slide' => 'swiper_gallery_slide',
-        'image_style_gallery_thumbnail' => 'swiper_gallery_thumbnail',
-        'hash_nav_replace_state' => FALSE,
-      ] + parent::defaultSettings();
+      'launcher_main_text' => 'Start gallery',
+      'launcher_footer_text' => 'Show all',
+      'show_preview_headline' => FALSE,
+      'preview_footer' => 'imageonly',
+      'image_style_preview_image' => 'swiper_gallery_preview',
+      'image_style_preview_thumbnail' => 'swiper_gallery_preview_thumbnail',
+      'image_style_gallery_slide' => 'swiper_gallery_slide',
+      'image_style_gallery_thumbnail' => 'swiper_gallery_thumbnail',
+      'hash_nav_replace_state' => FALSE,
+      'mobile_breakpoint' => 534,
+    ] + parent::defaultSettings();
   }
 
   /**
@@ -153,10 +154,10 @@ class SwiperGalleryFormatter extends EntityReferenceFormatterBase implements Con
     ];
 
     foreach ($image_styles as $image_style) {
-      list(,,$area, $type) = explode('_', $image_style);
+      list(, , $area, $type) = explode('_', $image_style);
       $form[$image_style] = [
         '#type' => 'select',
-        '#title' => $this->t('Image style: ' . ucfirst($area) . ' ' . ucfirst($type)),
+        '#title' => $this->t('Image style: %area %type', ['%area' => ucfirst($area), '%type' => ucfirst($type)]),
         '#default_value' => $this->getSetting($image_style),
         '#options' => image_style_options(FALSE),
       ];
@@ -167,6 +168,13 @@ class SwiperGalleryFormatter extends EntityReferenceFormatterBase implements Con
       '#title' => $this->t('Hash Navigation: replace url state'),
       '#description' => $this->t('Replace current url state with the new one instead of adding it to history when sliding through the gallery.'),
       '#default_value' => $this->getSetting('hash_nav_replace_state'),
+    ];
+
+    $form['mobile_breakpoint'] = [
+      '#type' => 'integer',
+      '#title' => $this->t('Mobile breakpoint'),
+      '#description' => $this->t('Make swiper slide vertical & remove thumbnails at this breakpoint.'),
+      '#default_value' => $this->getSetting('mobile_breakpoint'),
     ];
 
     return $form;
@@ -195,7 +203,6 @@ class SwiperGalleryFormatter extends EntityReferenceFormatterBase implements Con
     $preview_headline = $this->buildPreviewHeadline($items->getEntity()->label(), count($entities));
     $thumbnails = $this->buildImages($entities, 'swiper_gallery_thumbnail', $this->getSetting('image_style_gallery_thumbnail'));
     $referring_paragraph = $items->getEntity()->_referringItem->getEntity();
-    $settings_key = 'gallery-' . $referring_paragraph->id() . '-' . $this->gallery->id();
 
     $build = [
       '#theme' => 'swiper_gallery',
@@ -204,11 +211,7 @@ class SwiperGalleryFormatter extends EntityReferenceFormatterBase implements Con
           'swiper_gallery/swiper_gallery',
         ],
         'drupalSettings' => [
-          'swiperGallery' => [
-            $settings_key => [
-              'hashNavReplaceState' => (bool) $this->getSetting('hash_nav_replace_state'),
-            ],
-          ],
+          'swiperGallery' => $this->getDrupalSettings($items),
         ],
       ],
       '#slide_id_prefix' => $this->getSlideIdPrefix(),
@@ -219,6 +222,7 @@ class SwiperGalleryFormatter extends EntityReferenceFormatterBase implements Con
       '#thumbnails' => $thumbnails,
       '#gallery_id' => $this->gallery->id(),
       '#paragraph_id' => $referring_paragraph->id(),
+      '#viewmode' => $this->viewMode,
     ];
 
     CacheableMetadata::createFromObject($items->getEntity())->applyTo($build);
@@ -306,7 +310,7 @@ class SwiperGalleryFormatter extends EntityReferenceFormatterBase implements Con
       '#title' => $title,
       '#counter' => [
         '#type' => 'markup',
-        '#markup' => $count . ' ' . ($count > 1 ? t('Images') : t('Image')),
+        '#markup' => $count . ' ' . ($count > 1 ? $this->t('Images') : $this->t('Image')),
       ],
     ];
   }
@@ -319,7 +323,7 @@ class SwiperGalleryFormatter extends EntityReferenceFormatterBase implements Con
    *
    * @return array
    */
-  protected function buildPreview($media) {
+  protected function buildPreview(array $media) {
     $build = $this->buildImages([$media[0]], 'swiper_gallery_preview', $this->getSetting('image_style_preview_image'));
     $build = reset($build);
     $build['#launcher_main_text'] = $this->getSetting('launcher_main_text');
@@ -343,12 +347,30 @@ class SwiperGalleryFormatter extends EntityReferenceFormatterBase implements Con
   }
 
   /**
+   * Generate drupal settings which will be passed to the js.
+   *
+   * @return array
+   */
+  protected function getDrupalSettings() {
+    $settings_key = "gallery-{$this->viewMode}-{$this->gallery->id()}";
+
+    $settings = [
+      $settings_key => [
+        'hashNavReplaceState' => (bool) $this->getSetting('hash_nav_replace_state'),
+        'mobileBreakpoint' => $this->getSetting('mobile_breakpoint'),
+      ],
+    ];
+
+    return $settings;
+  }
+
+  /**
    * Gets the slide prefix.
    *
    * @return string
    */
   protected function getSlideIdPrefix() {
-    return 'slide-'. $this->gallery->id() . '-';
+    return 'slide-' . $this->gallery->id() . '-';
   }
 
 }
